4b0b07aebf13565ca98d1351d5a5a1a23e63006c
romdhaneSahar <romdhane_sahar@yahoo.com>
2022-08-27 12:44:10 +0200

    firstCommit

---
+++ TakiAppStorageBack/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Data;
+using System.Data.SqlClient;
+using TakiAppStorageBack.models;
+
+namespace TakiAppStorageBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class allFilesController : ControllerBase
+    {
+
+
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public allFilesController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+            select idFile,fileName,fileDate,fileSize,state
+             
+                     from dbo.files";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TakiAppStorageBack");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+  
+       
+       
+        
+       
+    }
+}
+
---
+++ TakiAppStorageBack/models/allFiles.cs
@@ -0,0 +1,15 @@
+﻿namespace TakiAppStorageBack.models
+{
+    public class allFiles
+    {
+        public int idFile { get; set; }
+
+        public string? FileName { get; set; }
+
+        public string? fileDate { get; set; }
+
+        public double? fileSize { get; set; }
+
+        public string? state { get; set; }
+    }
+}
